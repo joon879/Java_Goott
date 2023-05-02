9b55a1526d6d32dd134425f6d9e44d86a476f086
joon879 <joon879@gmail.com>
2023-05-02 18:19:35 +0900

    - 필드에 선언한 final 변수를 메서드에 활용

---
+++ j006/src/com/tech/w11/FinalExamTest.java
@@ -0,0 +1,12 @@
+package com.tech.w11;
+
+public class FinalExamTest {
+	public static void main(String[] args) {
+		FinalExam fe = new FinalExam();
+		int w = 100;
+		int h = 50;
+		
+		double result = fe.getArea(fe.TRIANGLE,w,h);
+		System.out.println("Area: "+result);
+	}
+}
