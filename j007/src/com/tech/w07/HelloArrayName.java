45e92125af531e76eb4cc5eb7c56a101e525ed5d
joon879 <joon879@gmail.com>
2023-05-03 17:49:19 +0900

    - args[] Test

---
+++ j007/src/com/tech/w07/HelloArrayName.java
@@ -0,0 +1,13 @@
+package com.tech.w07;
+
+public class HelloArrayName {
+	public static void main(String[] args) {
+		
+//		System.out.println(args[0]+"´Ô ¹Ý°©½À´Ï´Ù.");
+//		System.out.println(args[1]+"´Ô ¹Ý°©½À´Ï´Ù.");
+		
+		for (int i = 0; i < args.length; i++) {
+			System.out.println(args[i]+"´Ô ¹Ý°©½À´Ï´Ù.");
+		}
+	}
+}
