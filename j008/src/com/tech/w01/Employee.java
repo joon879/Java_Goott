c985fdfb2ea143da9b4f24dcc618a342f6956c5e
joon879 <joon879@gmail.com>
2023-05-07 23:49:55 +0900

    updae

--- j008/src/com/tech/w01/Employee.java
+++ j008/src/com/tech/w01/Employee.java
@@ -1,18 +1,20 @@
 package com.tech.w01;
 
-public class Employee {
-	private String empno; // 사번
-	private String ename; // 이름
-	private int pay; // 급여
+public abstract class Employee {
+	protected String empno; // 사번
+	protected String ename; // 이름
+	protected int pay; // 급여
 
 	public Employee(String empno, String ename, int pay) {
 		this.empno = empno;
 		this.ename = ename;
 		this.pay = pay;
 	}
-	public double getMonthPay() {
-		
-		return 0;
-	}
-
+	public abstract double getMonthPay();
+	
+	@Override
+	public String toString() {
+		return empno+":"+ename+":"+pay;
+	}//객체자체를 sys.out하면 자동으로 Object의 toString()함수를 호출한다.
+	//그래서 toString을 오버라이드해서 원하는 정보를 리턴하게 하여 사용 가능.
 }
--- j008/src/com/tech/w01/EmployeeTest.java
+++ j008/src/com/tech/w01/EmployeeTest.java
@@ -5,6 +5,9 @@ public class EmployeeTest {
 		// 사번 이름 연봉 보너스 등등 내용 전달하고 급여 산출
 
 		Employee emp1 = new RegularEmployee("E001", "홍길동", 3000, 500);
-		emp1.getMonthPay();
+		//E001", "홍길동", 3000만 RegularEmployee에 전달, 500은 Employee에
+		
+		System.out.println(emp1);
+		System.out.println(emp1.getMonthPay());
 	}
 }
