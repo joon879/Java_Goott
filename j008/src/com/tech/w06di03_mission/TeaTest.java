965c00461f0f5b0d52c773a0bd7513795961c64b
joon879 <joon879@gmail.com>
2023-05-08 20:39:19 +0900

    - 객체를 상위 인터페이스 객체로 생성: 상속받은 클래스 객체와 동일 개념으로.

--- j008/src/com/tech/w06di03_mission/TeaTest.java
+++ j008/src/com/tech/w06di03_mission/TeaTest.java
@@ -2,12 +2,12 @@ package com.tech.w06di03_mission;
 
 public class TeaTest {
 	public static void main(String[] args) {
-		BlackTea bt = new BlackTea();
-		GreenTea gt = new GreenTea();
-		YuzaTea yt = new YuzaTea();
+		KoreanTea bt = new BlackTea();
+		KoreanTea gt = new GreenTea();
+		KoreanTea yt = new YuzaTea();
 		
-		Tea tea = new Tea(new BlackTea());
-//		Tea tea = new Tea(bt);
+//		Tea tea = new Tea(new BlackTea());
+		Tea tea = new Tea(yt);
 		tea.teaType();
 	}
 }
