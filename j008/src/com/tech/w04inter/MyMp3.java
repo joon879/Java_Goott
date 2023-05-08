7e02ef07c953fdaf8068868d16194597f553355f
joon879 <joon879@gmail.com>
2023-05-08 10:06:23 +0900

    - update

--- j008/src/com/tech/w04inter/MyMp3.java
+++ j008/src/com/tech/w04inter/MyMp3.java
@@ -7,7 +7,9 @@ public interface MyMp3 {
 	//abstract 키워드 생략 가능. 애초에 인터페이스는 추상메소드들 모아둔거다.
 	public abstract void playMp3();
 	public void listenFm();
-	void viewPhoto(); //public 없으면 default.
+	void viewPhoto();
+	//추상메소드는 기본적으로 public
+	//생략하면 컴파일 시 자동으로 public abstract 추가.
 	
 //	public void aaa() {
 //		
