68bb6d11417ea9fc72deb85c49c356508bdac83b
joon879 <joon879@gmail.com>
2023-05-08 09:52:41 +0900

    - 부모 생성자와 자식생성자 처리 분배

--- j008/src/com/tech/w01/EmployeeTest.java
+++ j008/src/com/tech/w01/EmployeeTest.java
@@ -5,7 +5,8 @@ public class EmployeeTest {
 		// 사번 이름 연봉 보너스 등등 내용 전달하고 급여 산출
 
 		Employee emp1 = new RegularEmployee("E001", "홍길동", 3000, 500);
-		//E001", "홍길동", 3000만 RegularEmployee에 전달, 500은 Employee에
+		//E001", "홍길동", 3000만은 Employee(부모)에 전달, 
+		//500은 RegularEmployee(자식) 생성자에서 처리한다.
 		
 		System.out.println(emp1);
 		System.out.println(emp1.getMonthPay());
