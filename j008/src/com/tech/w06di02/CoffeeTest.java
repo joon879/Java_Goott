339848b1958cac073229f856d4e110b93380b93f
joon879 <joon879@gmail.com>
2023-05-09 10:00:07 +0900

    - 추가 설명

--- j008/src/com/tech/w06di02/CoffeeTest.java
+++ j008/src/com/tech/w06di02/CoffeeTest.java
@@ -3,6 +3,10 @@ package com.tech.w06di02;
 public class CoffeeTest {
 	public static void main(String[] args) {
 		
+//		HotAmericano hot = new HotAmericano();
+//		IceAmericano ice = new IceAmericano();
+		//아래와 동일. 이해하기엔 아래가 더 나은듯?
+		
 		Americano hot = new HotAmericano();
 		Americano ice = new IceAmericano();
 		//아래와 같이 객체를 생성하여 객체를 매개변수로 넘겨줄 수 있다.
