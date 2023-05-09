749b7907dc439f399dea74aba515258552a1c5a9
joon879 <joon879@gmail.com>
2023-05-09 10:11:11 +0900

    - update 객체생성 부분

--- j008/src/com/tech/w06di03_mission/TeaTest.java
+++ j008/src/com/tech/w06di03_mission/TeaTest.java
@@ -2,6 +2,12 @@ package com.tech.w06di03_mission;
 
 public class TeaTest {
 	public static void main(String[] args) {
+		
+//		BlackTea bt = new BlackTea();
+//		GreenTea gt = new GreenTea();
+//		YuzaTea yt = new YuzaTea();
+		//이렇게 해도 된다. but 아래가 더 직관적이다?
+		
 		KoreanTea bt = new BlackTea();
 		KoreanTea gt = new GreenTea();
 		KoreanTea yt = new YuzaTea();
